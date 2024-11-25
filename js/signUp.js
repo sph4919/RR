5976b7d7a0576da39ce190b90d0c51fd93d69dd7
sph4919 <patelsugnaykumar492003@gmail.com>
2024-11-25 11:47:18 -0600

    Adding js file for signUp

---
+++ js/signUp.js
@@ -0,0 +1,9 @@
+
+var fNameData = document.getElementById("fName")
+fNameData.addEventListener("change",signUpVerifyier)
+
+var mNameData = document.getElementById("mName")
+mNameData.addEventListener("change",signUpVerifyier)
+
+var lNameData = document.getElementById("lName")
+lNameData.addEventListener("change",signUpVerifyier)
